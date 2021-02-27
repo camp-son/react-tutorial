e3b0dc2ec41d022bc170d27170d670620ce5d275
camp-son <sjy2814@gmail.com>
2021-02-27 14:53:09 +0900

    함수형 컴포넌트

--- src/index.js
+++ src/index.js
@@ -4,16 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 ReactDOM.render(
-  <h1>Hello, world.</h1>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world.</h1>
+//       <h2>It is {new Date().toLocaleString()}</h2>
+//     </div>
+//   );
+
+//   ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+//   )
+// }
+
+// setInterval(tick, 1000);
+
+// function Welcome(props) {
+//   return <h1>Hello {props.name}</h1>;
+// }
+
+// ReactDOM.render(
+//   <Welcome name="Sara" />,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
