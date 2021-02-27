a6c9dbbd8ae728007cc035bb383ca3cb08029b10
camp-son <sjy2814@gmail.com>
2021-02-27 15:01:08 +0900

    컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './component/Welcome';
+import Comment from './component/Comment';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Welcome name="JY" />
       <Welcome name="YJ" />
       <Welcome name="Camp son" />
+      <Comment author={{name: 'camp-son', avatarUrl: 'https://avatars.githubusercontent.com/u/69656581?s=460&v=4'}} text="Hello world" date={new Date()}/>
     </div>
   );
 }
